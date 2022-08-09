d1c9119320ad7285c3e16837a05ec7c7f413fbb1
ram gopal <ramgopalsiddh.000@gmail.com>
2022-08-09 20:46:54 +0530

    created project for make todo app

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  # GET /about
+  get "about", to: "about#index"
+     # change  route url but don't change file name
+  # get "about-us", to: "about#index", as: :about
+  get "todo", to: "todo#index"
+
+  # root route 
+  # these are all three type can we define root route
+    # Defines the root path route ("/")
+  # get "/", to:"main#index"
+  # get "", to:"main#index"
+    # root "articles#index"
+  root to:"main#index"
+
+end
