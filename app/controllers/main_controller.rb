5b205f110f8aa5f9ae5dfd410da75cd03fa189b4
ram gopal <ramgopalsiddh.000@gmail.com>
2022-08-10 17:01:14 +0530

    add the show full errors messages in sign up form

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,6 @@
 class MainController < ApplicationController
     def index
-        flash.now[:notice] = "logged sucessfully"
-        flash.now[:alert] = "Invalid Email OR Password"
+       # flash.now[:notice] = "logged sucessfully"
+       # flash.now[:alert] = "Invalid Email OR Password"
     end
 end
