0ad99cf26f2af08ebba3829b4f1c9c33ac2d2d68
ram gopal <ramgopalsiddh.000@gmail.com>
2022-08-11 17:30:35 +0530

    password reset system complete

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -21,5 +21,15 @@ class PasswordResetsController < ApplicationController
 
     def update
         @user = User.find_signed!(params[:token], purpose: "password_reset" )
+        if @user.update(password_params)
+            redirect_to sign_in_path, notice:"Your password was reset sucessfully. please sign in "
+        else
+            render :edit
+        end
+    end
+
+    private
+    def password_params
+        params.require(:user).permit(:password, :password_confirmation)
     end
 end
